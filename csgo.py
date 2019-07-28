e80f80f708dd6c09b03caf7ca3ea2c1517e382a3
Avi Thour <thouravi@gmail.com>
2019-07-28 12:42:33 +0530

    Update csgo.py

--- csgo.py
+++ csgo.py
@@ -2,29 +2,29 @@ import random
 
 players = {
 #Team Liquid
-1 : {"name":"Stewie2k","rating": "105"},
-2 : {"name":"twistzz","rating": "114"},
-3 : {"name":"nitr0","rating": "102"},
-4 : {"name":"NAF","rating": "109"},
-5 : {"name":"EliGE","rating": "108"},
+1 : {"name":"Stewie2k","rating": 105},
+2 : {"name":"twistzz","rating": 114},
+3 : {"name":"nitr0","rating": 102},
+4 : {"name":"NAF","rating": 109},
+5 : {"name":"EliGE","rating": 108},
 #Vitality
-6 : {"name":"NBK-","rating": "104"},
-7 : {"name":"RpK","rating": "098"},
-8 : {"name":"apEX","rating": "105"},
-9 : {"name":"ALEX","rating": "101"},
-10 : {"name":"ZywOo","rating": "137"},
+6 : {"name":"NBK-","rating": 104},
+7 : {"name":"RpK","rating": 98},
+8 : {"name":"apEX","rating": 105},
+9 : {"name":"ALEX","rating": 101},
+10 : {"name":"ZywOo","rating": 137},
 #Astralis
-11 : {"name":"Xyp9x","rating": "104"},
-12 : {"name":"dupreeh","rating": "110"},
-13 : {"name":"gla1ve","rating": "101"},
-14 : {"name":"device","rating": "117"},
-15 : {"name":"Magisk","rating": "111"},
+11 : {"name":"Xyp9x","rating": 104},
+12 : {"name":"dupreeh","rating": 110},
+13 : {"name":"gla1ve","rating": 101},
+14 : {"name":"device","rating": 117},
+15 : {"name":"Magisk","rating": 111},
 #ENCE
-16 : {"name":"allu","rating": "110"},
-17 : {"name":"Aerial","rating": "116"},
-18 : {"name":"xseveN","rating": "100"},
-19 : {"name":"Aleksib","rating": "101"},
-20 : {"name":"sergej","rating": "119"},
+16 : {"name":"allu","rating": 110},
+17 : {"name":"Aerial","rating": 116},
+18 : {"name":"xseveN","rating": 100},
+19 : {"name":"Aleksib","rating": 101},
+20 : {"name":"sergej","rating": 119},
 }
 
 n = []
@@ -40,20 +40,20 @@ for i in players:
         b = random.choice(n)
         if a != b:
             if players[a]['rating'] > players[b]['rating']:
-                print(players[a]['name'],"wins against",players[b]['name'],"in a 1v1 with", int(players[a]['rating'])-int(players[b]['rating']), "HP.")
+                print(players[a]['name'],"wins against",players[b]['name'],"in a 1v1 with", players[a]['rating']-players[b]['rating'], "HP.")
                 n.remove(b)
             else:
-                print(players[b]['name'],"wins against",players[a]['name'],"in a 1v1 with", int(players[b]['rating'])-int(players[a]['rating']), "HP.")
+                print(players[b]['name'],"wins against",players[a]['name'],"in a 1v1 with", players[b]['rating']-players[a]['rating'], "HP.")
                 n.remove(a)
         else:
             a = random.choice(n)
             b = random.choice(n)
             if a != b:
                 if players[a]['rating'] > players[b]['rating']:
-                    print(players[a]['name'],"wins against",players[b]['name'],"in a 1v1 with", int(players[a]['rating'])-int(players[b]['rating']), "HP.")
+                    print(players[a]['name'],"wins against",players[b]['name'],"in a 1v1 with", players[a]['rating']-players[b]['rating'], "HP.")
                     n.remove(b)
                 else:
-                    print(players[b]['name'],"wins against",players[a]['name'],"in a 1v1 with", int(players[b]['rating'])-int(players[a]['rating']), "HP.")
+                    print(players[b]['name'],"wins against",players[a]['name'],"in a 1v1 with", players[b]['rating']-players[a]['rating'], "HP.")
                     n.remove(a)
     else:
         print("\n")
